122d6d2ec9247337ed6146d7df00636bf97971f0
luquetaroblox <luquetaateste@gmail.com>
2025-09-09 11:46:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+import os
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    target_url = f"http://213.142.135.46:9999/{path}"
     response = requests.request(
-        url="http://213.142.135.46:9999" + request.path,
         method=request.method,
+        url=target_url,
         data=request.get_data(),
         headers=request.headers
     )
     return response.text, response.status_code, {"Content-Type": response.headers.get("Content-Type")}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
